f00f63883eadba20e760f323daabe10b28fe59fd
vikas pokhriyal <pokhriyalvikas940@gmail.com>
2024-09-23 15:39:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Country from './pages/Country'
+import ErrorPage from './pages/ErrorPage'
+import AppLayout from './components/layout/AppLayout'
+import CountryDetails from './components/layout/CountryDetails'
+
+
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AppLayout/>,
+      errorElement:<ErrorPage/>,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+
+        {
+          path: 'about',
+          element: <About />,
+        },
+
+        {
+          path: 'contact',
+          element: <Contact />,
+        },
+
+        {
+          path: 'country',
+          element: <Country />,
+        },
+        {
+          path: 'country/:id',
+          element: <CountryDetails/>,
+        }
+      ]
+    }
+
+  ])
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
